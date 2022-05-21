bd737a05a007538be05dd3e1536e0beb6a28d9db
tahachoura <taha.choura@supcom.tn>
2022-05-21 21:14:52 +0200

    Update 83.Remove Duplicates from Sorted List.py

--- Linked List/83.Remove Duplicates from Sorted List.py
+++ Linked List/83.Remove Duplicates from Sorted List.py
@@ -1,10 +1,12 @@
-# Problem:
+```# Problem:
 ![image](https://user-images.githubusercontent.com/11164303/169665991-5ef6b618-265f-4fcb-bdb4-bbe58f04b582.png)
 
 
 -------- 
 # Solution:
-```python
+```
+
+
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         current=head
@@ -12,5 +14,3 @@ class Solution:
             if current.val==current.next.val: current.next=current.next.next
             else: current=current.next
         return head
-```
-
